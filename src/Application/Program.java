009714e2a458234ea0b365be338a171826f42f9e
Israel Hamdan <IsraelSantos_20K@outlook.com>
2023-12-20 23:07:12 -0300

    first commit

---
+++ src/Application/Program.java
@@ -0,0 +1,9 @@
+package Application;
+
+public class Program {
+    public static void main(String[] args) {
+        double ladoAx, ladoBx, ladoCx, ladoAy,ladoBy, ladoCy;
+
+        System.out.println();
+    }
+}
